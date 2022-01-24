92cdd0cd7d8dcc362810fcce56cf2ce3c401b218
Matt Pityo <mpityo@gmail.com>
2022-01-23 19:22:44 -0500

    add ability to change date by clicking it and saving to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,6 +56,7 @@ $(".list-group").on("click", "p", function() {
   textInput.trigger("focus");
 });
 
+// outside of the text area that was created above was clicked
 $(".list-group").on("blur", "textarea", function() {
   // get textarea's current value/text
   var text = $(this)
@@ -85,6 +86,57 @@ $(".list-group").on("blur", "textarea", function() {
   
   // replace textarea with p element
   $(this).replaceWith(taskP);
+});
+
+// date clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+  
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// outside of the input area that was created above was clicked
+$(".list-group").on("blur", "input[type='text']", function () {  
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+  
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localStorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
 });
 
 
